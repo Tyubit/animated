85f0e63ebd4f1ad719507c315a39c1e2869464d9
Eli Kovalev <elisey.k01@gmail.com>
2025-05-28 15:43:54 -0700

    about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/Headers";
 import { Hero } from "@/components/Hero";
 import { Services } from "@/components/Services";
+import { About } from "@/components/About";
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <Services/>
+      <Services />
+      <About/>
     </>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export const About = () => {
 return (
     <div>
-        <h4>Since 2017</h4>
-        <p>We are award winning design studio</p>
-        <h1>From London, UK.</h1>
+        <div className='flex flex-col items-center'>
+            <h4 className='font-bold'>Since 2017</h4>
+            <p className='font-serif text-[18px]'>We are award winning design studio</p>
+        </div>
+        <h1 className="text-[170px] font-bold">From <br/>London, UK.</h1>
         <p>We hold the conviction that excellent design transcends mere appearance — it serves as a vital medium for communication and engagement.</p>
     </div>
 )
